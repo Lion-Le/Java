b1fd6c310193597f7c4f5c4ff9bb82574bd8ee1b
Lion-Le <lion.le.tw@gmail.com>
2024-01-25 21:22:29 +0800

    Native method

--- Native/Hello.java
+++ Native/Hello.java
@@ -1,8 +1,10 @@
 public class Hello{
 	static{
+		//載入C++產出的DLL檔
 		System.loadLibrary("Hello");
 	}
 	
+	//呼叫C++對應的Java_Hello_callC方法
 	public native String callC();
 	
 	public static void main(String argv[]){
